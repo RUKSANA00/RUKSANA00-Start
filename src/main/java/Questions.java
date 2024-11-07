ffec1dded3a855637fdc48952c91e15127c4268a
RUKSANA00 <shaikruksanachandhini@gmail.com>
2024-11-07 13:29:05 +0530

    Done

--- src/main/java/Questions.java
+++ src/main/java/Questions.java
@@ -4,12 +4,14 @@
  */
 public class Questions {
     int question1(){
+        
+        
         /* 
          * Replace the 0 in the return statement with the answer you believe to be true for the question below
          * 
          * How many variables are in the class "Vehicle"?
          */
-        return 0;
+        return 3;
     }
 
     int question2(){
@@ -18,14 +20,14 @@ public class Questions {
          * 
          * What line number represents the end of the "accelerate" method? 
          */
-        return 0;
+        return 14;
     }
 
     String question3(){
         /* 
          * What datatype is expected to be returned in the "honk" method?
          */
-        return "replace this text with your answer";
+        return "String";
     }
     
 }
